c2bc8f720e8c319e95cb2f346d85f72192affed7
titomundo <tito.alberto.mundo@gmail.com>
2025-12-09 22:53:04 -0600

    add -1 to loop to avoid out of index error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,11 +12,17 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	int aux, swp = 1;
 
+	if (size < 2)
+	{
+		print_array(array, size);
+		return;
+	}
+
 	while (swp != 0)
 	{
 		swp = 0;
 
-		for (i = 0; i < size; i++)
+		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
